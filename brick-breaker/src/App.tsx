91ab632e30d7b2ab7edf7412df7bc066acd76948
David Neil <dhughneil@protonmail.com>
2023-12-13 17:01:02 -0600

    Shifted everything from index.html to App.tsx

--- brick-breaker/src/App.tsx
+++ brick-breaker/src/App.tsx
@@ -4,14 +4,13 @@ import './App.css'
 const App = () => {
   return (
     <>
+    
     <h3>Brick Breaker Redux</h3>
-    {/* <div id="root">Hello World!</div>
-    <canvas id="canvas" width="500" height="300"></canvas>
-    <h3>Brick Breaker Redux</h3>
-    <p>Mouse moves platform &bull; Press any key to pause</p>
-      <button onClick={() => console.log("Clicked")}>Play again</button> 
-      <button id="reloadButton" onClick="reload()">Play again</button>
-      <div id="score"></div> */}
+    <div><canvas id="canvas" width="500" height="300"></canvas></div>
+    <div><p>Mouse moves platform &bull; Press any key to pause</p></div>
+    <button id="reloadButton" onClick={() => console.log("Clicked")}>Play again</button> 
+    {/*   <button id="reloadButton" onClick="reload()">Play again</button> */}
+      <div id="score"></div>
     </>
         )
       }

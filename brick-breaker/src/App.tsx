d1fefd785c6b7a628dc588474eb90f84d02ec2d3
David Neil <dhughneil@protonmail.com>
2023-12-12 21:05:02 -0600

    Initial setup and changes

--- brick-breaker/src/App.tsx
+++ brick-breaker/src/App.tsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    {/* <div id="root">Hello World!</div> */}
+    <h1>Brick Breaker Redux</h1>
+    <canvas id="canvas" width="500" height="300"></canvas>
+    <p>Mouse moves platform &bull; Press any key to pause</p>
+      {/* </p></p><button onclick="reload()">Play again</button>  */}
+      {/* <button id="reloadButton" onClick="reload()">Play again</button> */}
+      <div id="score"></div>
     </>
-  )
-}
+        )
+      }
 
-export default App
+        export default App
--- brick-breaker/src/Components/Board.tsx
+++ brick-breaker/src/Components/Board.tsx
@@ -2,3 +2,6 @@
 
 // Define the size and boundries of the board
 // render walls and ceiling
+
+// this code came from the HTML of CodePen
+// <button id="reloadButton" onclick="reload()">Play again</button>
